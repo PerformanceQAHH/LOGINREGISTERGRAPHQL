a756854dc9a18245ec172c2a2fd90c1058345b35
PerformanceQAHH <hherrera@iteggo.com>
2025-07-03 12:10:45 +0000

    Nuevo flujo incluido completo

--- dist/resolvers/mutation.js
+++ dist/resolvers/mutation.js
@@ -1,47 +1,84 @@
+
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const datetime_1 = require("../lib/datetime");
-const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const { ObjectId } = require('mongodb');
+const bcrypt = require("bcryptjs");
+const { Datetime } = require("../lib/datetime");
+
 const mutation = {
-    Mutation: {
-        async register(_, { user }, { db }) {
+        Mutation: {
+            async register(_, { user }, { db }) {
             const userCheck = await db.collection('users').findOne({ email: user.email });
             if (userCheck !== null) {
                 return {
-                    status: false,
-                    message: `Usuario NO registrado porque ya existe el usuario ${user.email}`,
-                    user: null
-                };
-            }
+                status: false,
+                message: `Usuario NO registrado porque ya existe el usuario ${user.email}`,
+                user: null
+            };
+        }
+
             const lastUser = await db.collection('users').find()
-                .limit(1).sort({ registerDate: -1 }).toArray();
-            if (lastUser.length === 0) {
-                user.id = 1;
-            }
-            else {
-                user.id = lastUser[0].id + 1;
-            }
-            user.password = bcryptjs_1.default.hashSync(user.password, 10);
-            user.registerDate = new datetime_1.Datetime().getCurrentDateTime();
+            .limit(1).sort({ registerDate: -1 }).toArray();
+
+            user.id = lastUser.length === 0 ? 1 : lastUser[0].id + 1;
+            user.password = bcrypt.hashSync(user.password, 10);
+            user.registerDate = new Datetime().getCurrentDateTime();
+
             return await db.collection('users').insertOne(user)
-                .then((result) => {
+            .then(() => ({
+            status: true,
+            message: `Usuario ${user.name} ${user.lastname} añadido correctamente`,
+                user
+            }))
+            .catch(() => ({
+            status: false,
+            message: `Usuario NO añadido correctamente`,
+                user: null
+                }));
+            },
+
+        async createNote(_, { content }, { db, user }) {
+            if (!user) {
                 return {
-                    status: true,
-                    message: `Usuario ${user.name} ${user.lastname} añadido correctamente`,
-                    user
-                };
-            })
-                .catch((err) => {
-                return {
-                    status: false,
-                    message: `Usuario NO añadido correctamente`,
-                    user: null
-                };
-            });
+                status: false,
+                message: "No autorizado",
+                note: null
+            };
+        }
+
+        const newNote = {
+            content,
+            createdAt: new Date().toISOString(),
+            createdBy: new ObjectId(user._id)
+        };
+
+        const result = await db.collection('notes').insertOne(newNote);
+
+        return {
+        status: true,
+        message: "Nota creada correctamente",
+        note: {
+        id: result.insertedId,
+        ...newNote
+        }
+    };
+},
+
+        logout(_, __, { user }) {
+        if (!user) {
+            return {
+            status: false,
+            message: "No autorizado",
+            user: null
+        };
+    }
+
+    return {
+            status: true,
+            message: "Sesión cerrada correctamente",
+            user: null
+            };
         }
     }
 };
-exports.default = mutation;
+
+module.exports = mutation;
