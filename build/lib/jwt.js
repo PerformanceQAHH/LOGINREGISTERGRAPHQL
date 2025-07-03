6afe03239775797ff79633960eda7e5f8f33f711
PerformanceQAHH <hherrera@iteggo.com>
2025-07-03 18:10:35 +0000

    Usar process.env.JWT_SECRET para firmar tokens

--- build/lib/jwt.js
+++ build/lib/jwt.js
@@ -7,14 +7,14 @@ const constants_1 = require("../config/constants");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 class JWT {
     constructor() {
-        this.secretKey = constants_1.SECRET_KEY;
+        this.secretKey = process.env.JWT_SECRET || 'tu_clave_secreta';
     }
     sign(data) {
-        return jsonwebtoken_1.default.sign({ user: data.user }, this.secretKey, { expiresIn: 24 * 60 * 60 });
+        return jsonwebtoken.sign({ user: data.user }, this.secretKey, { expiresIn: 24 * 60 * 60 });
     }
     verify(token) {
         try {
-            return jsonwebtoken_1.default.verify(token, this.secretKey);
+            return jsonwebtoken.verify(token, this.secretKey);
         }
         catch (e) {
             return 'La autenticación del token es inválida. Por favor, inicia sesión para obtener un nuevo token';
